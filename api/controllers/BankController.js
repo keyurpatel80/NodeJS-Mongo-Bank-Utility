ef5267eac3c7d989aa1e3aa6ca063a6322dca478
Keyur Patel <keyurpatel80@gmail.com>
2017-08-27 22:54:29 -0500

    Added API endpoint for listing Accounts and filtering by Currency

--- api/controllers/BankController.js
+++ api/controllers/BankController.js
@@ -7,22 +7,51 @@ var BankTask = mongoose.model('Bank_data');
 
 
 exports.list_all_data = function(req, res, next) {
-  BankTask.paginate({},{ page: req.query.page, limit: req.query.limit }, function(err, accounts) {
+  var param = {};
+  if (req.query.first_name) {
+    BankTask.paginate({first_name: req.query.first_name},{ page: req.query.page, limit: req.query.limit }, function(err, accounts) {
     res.json({
+          pageCount: accounts.pages,
+          itemCount: accounts.limit,
           has_more: paginate.hasNextPages(req)(accounts.pages),
           data: accounts
         }
         );
     });
+  }
+  else{
+   BankTask.paginate({},{ page: req.query.page, limit: req.query.limit }, function(err, accounts) {
+    res.json({
+          pageCount: accounts.pages,
+          itemCount: accounts.limit,
+          has_more: paginate.hasNextPages(req)(accounts.pages),
+          data: accounts
+        }
+        );
+    })
+  }
 };
 
 
 exports.list_by_account_id = function(req, res) {
-  BankTask.find({_id: new ObjectId(req.params.id)}, function(err, accounts) {
+    BankTask.find({_id: new ObjectId(req.params.id)}, function(err, accounts) {
     res.json(accounts);
     });
 };
 
+exports.list_id_by_accounts = function(req, res) {
+   console.log(req.query.currency);
+   if (req.query.currency){
+      BankTask.aggregate({$match:{_id: new ObjectId(req.params.id)}}, {$unwind: "$accounts"}, {$match: {"accounts.currency": req.query.currency}}, function(err, accounts) {
+      res.json(accounts);
+      });
+   }
+   else{
+     BankTask.findOne({_id: new ObjectId(req.params.id)}, function(err, accounts) {
+      res.json(accounts.accounts);
+      });
+   }
+  };
 
 exports.create_a_account = function(req, res) {
   var new_account = new BankTask(req.body);
--- api/routes/BankRoutes.js
+++ api/routes/BankRoutes.js
@@ -3,12 +3,15 @@ module.exports = function(app) {
   var bank_data_list = require('../controllers/BankController');
 
   // bank_data_list Routes
-  app.route('/accounts/')
+  app.route('/customers/')
     .get(bank_data_list.list_all_data)
     .post(bank_data_list.create_a_account);
 
-  app.route('/accounts/:id')
+  app.route('/customers/:id')
      .get(bank_data_list.list_by_account_id);
+
+  app.route('/customers/:id/:accounts')
+     .get(bank_data_list.list_id_by_accounts);
 
   //app.route('/accounts/:accountId')
  //   .get(bank_data_list.read_a_account)
